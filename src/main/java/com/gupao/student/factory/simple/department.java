27471f2a06c5b40e253ad82dac95aad8fd0d1a22
zhuochen <zhuochen1990@163.com>
2019-04-17 10:12:31 +0800

    重新提交内同

---
+++ src/main/java/com/gupao/student/factory/simple/department.java
@@ -0,0 +1,11 @@
+package com.gupao.student.factory.simple;/**
+ * Created by zhuochen on 2019/4/16.
+ */
+
+/**
+ * @author zhuochen
+ * @comment
+ * @date 2019/4/16
+ */
+public interface department {
+}
